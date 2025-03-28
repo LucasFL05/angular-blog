51bb56e2046416a7dbf0973822cf535b9d7533af
Lucas Ferreira Lourenço <141689321+LucasFL05@users.noreply.github.com>
2025-03-28 10:12:10 -0300

    components createds with styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuTitleComponent, HomeComponent, BigCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
