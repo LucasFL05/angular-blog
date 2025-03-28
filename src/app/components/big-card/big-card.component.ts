46f41d433b006295daa9db2a5fde631b21a1fde7
Lucas Ferreira Lourenço <141689321+LucasFL05@users.noreply.github.com>
2025-03-28 12:01:06 -0300

    component dynamic properties

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
+  @Input()
+  photoCover:String =""
+  @Input()
+  cardTitle:String=""
+  @Input()
+  cardDescription:String=""
 
 }
